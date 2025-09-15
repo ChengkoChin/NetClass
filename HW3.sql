a0a1bb079a39cfdae7cfb7caf070f2c086cceca8
ChengkoChin <chengko9991@gmail.com>
2025-09-15 13:01:02 -0700

    .

--- HW3.sql
+++ HW3.sql
@@ -81,28 +81,56 @@ AND c.City is not NULL AND o.ShipCity is not NULL
 
 
 --8. List 5 most popular products, their average price, and the customer city that ordered most quantity of it.
-SELECT * FROM Customers
-SELECT * FROM Orders
-SELECT * FROM [Order Details]
-SELECT * FROM Products
+WITH Prod AS (
+	SELECT p.ProductID, p.ProductName, SUM(od.Quantity) AS TotalOrders, CAST(AVG(od.UnitPrice) AS DECIMAL(7, 2)) AS AVGPRICE
+	FROM Products p
+	JOIN [Order Details] od ON p.ProductID = od.ProductID
+	GROUP BY p.ProductID, p.ProductName
+), 
+CityRank AS (
+	SELECT p.ProductID, c.City, 
+	ROW_NUMBER() OVER (PARTITION BY p.ProductID ORDER BY SUM(od.Quantity) DESC) AS Ranking
+	FROM Products p
+	JOIN [Order Details] od ON p.ProductID = od.ProductID
+	JOIN Orders o ON od.OrderID = o.OrderID
+	JOIN Customers c ON o.CustomerID = c.CustomerID
+	GROUP BY p.ProductID, c.City)
 
-SELECT TOP 5 p.ProductName, AVG(od.UnitPrice) AS AVGprice, c.City
-FROM Products p
-JOIN [Order Details] od ON p.ProductID = od.ProductID
-JOIN Orders o ON od.OrderID = o.OrderID
-JOIN Customers c ON o.CustomerID = c.CustomerID
-GROUP BY p.ProductName
-ORDER BY SUM(od.Quantity) DESC
-
+SELECT TOP 5 p.ProductName, p.AVGPRICE, cr.City FROM Prod p
+JOIN CityRank cr ON p.ProductID = cr.ProductID AND cr.Ranking = 1
+ORDER BY p.TotalOrders DESC
 
 
 --9.List all cities that have never ordered something but we have employees there.
---a.      Use sub-query
+	--a. Use sub-query
+	SELECT DISTINCT City FROM Employees
+	WHERE City NOT IN ( SELECT DISTINCT ShipCity FROM Orders )
 
---b.      Do not use sub-query
+	--b. Do not use sub-query
+	SELECT DISTINCT City FROM Employees
+	EXCEPT
+	SELECT DISTINCT ShipCity FROM Orders
 
 
---10.List one city, if exists, that is the city from where the employee sold most orders (not the product quantity) is, and also the city of most total quantity of products ordered from. (tip: join  sub-query)
+/* 10.List one city, if exists, that is the city from where the employee sold most orders (not the product quantity) is,
+and also the city of most total quantity of products ordered from. 
+(tip: join  sub-query) */
+WITH TopEmployeeCity AS (
+SELECT TOP 1 e.City FROM Employees e
+JOIN Orders o ON e.EmployeeID = o.EmployeeID
+GROUP BY e.City
+ORDER BY COUNT(o.OrderID) DESC ), TopCity AS (
+SELECT TOP 1 c.City FROM Customers c
+JOIN Orders o ON c.CustomerID = o.CustomerID
+JOIN [Order Details] od ON o.OrderID = od.OrderID
+GROUP BY c.City
+ORDER BY SUM(od.Quantity) DESC )
+
+SELECT e.City FROM TopEmployeeCity e
+JOIN TopCity c ON e.City = c.City
 
 
 --11.How do you remove the duplicates record of a table?
+/*
+I think I could use DISTINCT to get a new table, drop the old table, and rename the new table.
+*/
